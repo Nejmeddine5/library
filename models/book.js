363ece1afd2443fbdbf55d90fecac03b34199214
Nejmeddine5 <meskaoui.nejmeddine5@gmail.com>
2025-03-02 23:30:31 +0100

    page login et register

---
+++ models/book.js
@@ -0,0 +1,27 @@
+const { rejects } = require('assert')
+const mongoose=require('mongoose')
+const { resolve } = require('path')
+
+var schemaBook=mongoose.Schema({
+    _id:String,
+    title:String,
+    description:String,
+    author:String,
+    price:Number,
+    image:String
+
+})
+
+var Book=mongoose.model('book',schemaBook)
+var url='mongo://localhost:27017/library'
+exports.getallbooks=()=>{
+    return new Promise((resolve,reject)=>{
+        mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopologie:true}).then(()=>{
+            return Book.find({})
+
+        }).then(books=>{
+            mongoose.disconnect()
+            resolve(books)
+        }).catch(err=>reject(err))
+    })
+}
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express=require('express')
 const app=express()
 const path=require('path')
-
+const mongosse=require('mongose')
 
 
 app.use(express.static(path.join(__dirname,'assets')))
 app.set('view engine','ejs')
 app.set('views','views')
-
-
-
-
-
-
 
 
 app.get('/',(req,res,next)=>{
@@ -29,6 +23,14 @@ app.get('/about',(req,res,next)=>{
 
 app.get('/books',(req,res,next)=>{
     res.render('books')
+})
+
+app.get('/login',(req,res,next)=>{
+    res.render('login')
+})
+
+app.get('/register',(req,res,next)=>{
+    res.render('register')
 })
 
 app.listen(3100,()=>console.log('server run on port 3100'))
